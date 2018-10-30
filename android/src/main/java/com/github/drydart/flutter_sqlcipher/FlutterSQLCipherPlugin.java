e29ca17920bcfc9e4ec00bb31d528cd48f186104
Arto Bendiken <arto@bendiken.net>
2018-10-30 18:10:23 +0200

    Initialized SQLCipher on plugin registration.

--- android/src/main/java/com/github/drydart/flutter_sqlcipher/FlutterSQLCipherPlugin.java
+++ android/src/main/java/com/github/drydart/flutter_sqlcipher/FlutterSQLCipherPlugin.java
@@ -5,12 +5,15 @@ package com.github.drydart.flutter_sqlcipher;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+import net.sqlcipher.database.SQLiteDatabase;
+
 /** FlutterSQLCipherPlugin */
 public class FlutterSQLCipherPlugin {
 
   /** Plugin registration. */
   public static void registerWith(final Registrar registrar) {
     assert(registrar != null);
+    SQLiteDatabase.loadLibs(registrar.context());
 
     (new MethodChannel(registrar.messenger(), SQLCipherMethodHandler.CHANNEL))
       .setMethodCallHandler(new SQLCipherMethodHandler());
