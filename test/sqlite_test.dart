dfa02f004065b3acf5de7a5fe98c6b4f41e4c703
Arto Bendiken <arto@bendiken.net>
2018-11-15 16:36:35 +0200

    Fixed the test suite runner.

--- test/sqlite_test.dart
+++ test/sqlite_test.dart
@@ -1,9 +1,11 @@
 /* This is free and unencumbered software released into the public domain. */
 
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_sqlcipher/sqlite.dart';
 
 void main() {
-  // TODO
+  group("sqlite", () {
+    // TODO
+  });
 }
